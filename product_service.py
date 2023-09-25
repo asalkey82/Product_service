138550f11c190a484fe5a5654d7e8651affac4a0
asalkey82 <89543219+asalkey82@users.noreply.github.com>
2023-09-24 23:32:13 -0400

    Update product_service.py

--- product_service.py
+++ product_service.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+#Sample data
 products = [
     {"id": 1, "name": "banana", "price": 0.23, "quantity": 5},
     {"id": 2, "name": "apple", "price": 0.25, "quantity": 10},
     {"id": 3, "name": "beef", "price": 8.00, "quantity": 9}
 ]
 
+#Endpoint 1: Get all products from the store with addtional details
 @app.route('/products', methods=['GET'])
 def get_products():
     return jsonify({"products": products})
-    
+
+#Endpoint 2: Get details for specifc product. Give name, quantity, and price.
 @app.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
     product = next((product for product in products if product["id"] == product_id), None)
@@ -20,6 +23,7 @@ def get_product(product_id):
     else:
         return jsonify({"error": "Product not found"}), 404
 
+#Enpoint 3: Add new product to the store
 @app.route('/products', methods=['POST'])
 def add_products():
     new_product = {
@@ -31,6 +35,7 @@ def add_products():
     products.append(new_product)
     return jsonify({"message": "Product added", "product": new_product}), 201
 
+#Endpoint 4: Additional endpoint to take product out of store. Reflects the amount in user cart.
 @app.route('/remove/<int:product_id>', methods=['POST'])
 def remove_quantity(product_id):
     product = next((product for product in products if product["id"] == product_id), None)
@@ -38,6 +43,7 @@ def remove_quantity(product_id):
         products[product_id-1]['quantity'] -= 1
         return jsonify({"message": "Product added to cart"})
 
+#Endpoint 5: Additional endpoint to add product back to the store. Reflects the amount in user cart. 
 @app.route('/add/<int:product_id>', methods=['POST'])
 def add_quantity(product_id):
     product = next((product for product in products if product["id"] == product_id), None)
